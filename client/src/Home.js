25d5a5f48a7c7d3996c014980205dfe9beeb3d28
John Atay <johncanaty@gmail.com>
2019-08-02 17:16:17 -0700

    pulltoday

--- client/src/Home.js
+++ client/src/Home.js
@@ -9,6 +9,8 @@ class Home extends Component {
         products: []
     };
 
+    productRender = true;
+
     handleInputChange = event => {
         const { name, value } = event.target;
         this.setState({
@@ -19,72 +21,88 @@ class Home extends Component {
     submitSearch = event => {
         var searchTerm = this.state.keyword.replace(/ +/g, "+");
 
+        this.productRender = false;
         API.scrapeProduct(searchTerm)
             .then(res => {
                 console.log(res.data.length);
                 console.log(res.data);
 
                 this.setState({ products: res.data, keyword: "" });
-
                 // this.props.history.replace('/');
             })
             .catch(err => alert(err));
     }
 
+
     saveSearch = product => {
 
         API.saveProduct(product)
-        .then((res) => {
-            const products = this.state.products.filter(item => product.id !== item.id);
-            
-            this.setState({ products: products });
-        })
+            .then((res) => {
+                const products = this.state.products.filter(item => product.id !== item.id);
+                this.setState({ products: products });
+
+            })
+        // this.scrolldown();
+    }
+
+componentDidUpdate(){
+    if (this.state.products.length > 0 && !this.productRender){
+        this.scrolldown();
+        this.productRender = true;
+        
+    }
+}
+    scrolldown() {
+        window.scroll({
+            top: 500, // could be negative value
+            left: 0,
+            behavior: 'smooth'
+        });
     }
 
     render() {
         return (
             <div>
-            <div className="hero-image">
-                <img className="hero-image fluid" src="../images/Home-header.png" alt="header" />
-                <div className="hero-text">
-                    <div className="wrap clearfix">
-                        <div className="search">
-                            <input
-                                value={this.state.keyword}
-                                name="keyword"
-                                type="text"
-                                className="searchTerm"
-                                placeholder="What are you looking for?"
-                                onChange={this.handleInputChange}
-                            />
-                            <button type="submit" className="searchButton" onClick={this.submitSearch}>
-                                <i className="fa fa-search"></i>
-                            </button>
+                <div className="hero-image">
+                    <img className="hero-image fluid" src="../images/Home-header.png" alt="header" />
+                    <div className="hero-text">
+                        <div className="wrap clearfix">
+                            <div className="search">
+                                <input
+                                    value={this.state.keyword}
+                                    name="keyword"
+                                    type="text"
+                                    className="searchTerm"
+                                    placeholder="What are you looking for?"
+                                    onChange={this.handleInputChange}
+                                />
+                                <button type="submit" className="searchButton" onClick={this.submitSearch}>
+                                    <i className="fa fa-search"></i>
+                                </button>
+                            </div>
                         </div>
+
                     </div>
-
                 </div>
-            </div>
-            
-                    <div className="container search-result py-3">
-                        <h5>Products found...</h5>
-                        <div className="card-columns">
-                            {this.state.products.map(product => (
-                                <div className="card">
-                                    <img src={product.image} className="card-img-top product-img" alt="..." />
 
-                                    <div className="card-body">
-                                        <h5 className="card-title">{product.title}</h5>
-                                        <div className="clearfix">
-                                            <p className="card-text float-left">{product.price}</p>
-                                            <button className="btn-add float-right" onClick={() => this.saveSearch(product)}>Add</button>
-                                        </div>
+                <div className="container search-result py-3">
+                    <div className="card-columns">
+                        {this.state.products.map(product => (
+                            <div className="card">
+                                <img src={product.image} className="card-img-top product-img" alt="..." />
+
+                                <div className="card-body">
+                                    <h5 className="card-title">{product.title}</h5>
+                                    <div className="clearfix">
+                                        <p className="card-text float-left">{product.price}</p>
+                                        <button className="btn-add float-right" onClick={() => this.saveSearch(product)}>Add</button>
                                     </div>
                                 </div>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
                     </div>
-                    </div>
+                </div>
+            </div>
         );
     }
 }
