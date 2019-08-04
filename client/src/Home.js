17bd71f051ab566056efe6f7b57179b477a2ddbd
John Atay <johncanaty@gmail.com>
2019-08-04 16:11:23 -0700

    margin done size stable header image on mobile devices vheader video finished

--- client/src/Home.js
+++ client/src/Home.js
@@ -25,7 +25,7 @@ class Home extends Component {
     handleKeyDown = event => {
         if (event.key === 'Enter') {
             console.log('Enter pressed');
-            { this.submitSearch() };
+            this.submitSearch();
         }
     }
 
@@ -85,18 +85,15 @@ class Home extends Component {
 
 
     render() {
-        return (
+        return ([
+
             <div>
-                 <div style={{
-                        width: '1920px',
-                        height: '1080px',
-                       
-                    }}>
-                        <video autoplay muted loop poster="../images/video.mp4" src="../images/\video.mp4" />
-                    </div>
-                <div className="hero-image">
-                    {/* <img className="hero-image fluid" src="../images/Home-header.png" alt="header" /> */}
-                    <div className="hero-text">
+                <div className="hero-image hero-image header-image fluid">
+                <img id="headerImg "className="hero-image header2 fluid" src="/images/header.png" alt="header" />
+                    <video className="hero-image header-image fluid" autoPlay muted id="myVideo">
+                        <source src={process.env.PUBLIC_URL + "/images/video2.mp4"} type="video/mp4" />
+                    </video>
+                        <div className="hero-text" >
                         <div className="wrap clearfix">
                             <div className="search">
                                 <input
@@ -115,13 +112,11 @@ class Home extends Component {
                         </div>
                     </div>
                 </div>
-
                 <div className="container search-result py-3">
                     <div className="card-columns">
                         {this.state.products.map(product => (
                             <div className="card" key={product.id}>
                                 <img src={product.image} className="card-img-top product-img" alt="..." />
-
                                 <div className="card-body">
                                     <h5 className="card-title">{product.title}</h5>
                                     <div className="clearfix">
@@ -135,7 +130,7 @@ class Home extends Component {
                     </div>
                 </div>
             </div>
-        );
+        ]);
     }
 }
 
